a0a657867269844840393ffb7f96996cbe7b1046
shadymansour9 <110628347+shadymansour9@users.noreply.github.com>
2024-08-12 08:43:15 +0300

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://netorkingserver-jqlempxcq-shady-mansours-projects.vercel.app/', // Ensure this matches your backend port
+  baseURL: 'https://netorking-new-server.vercel.app/', // Ensure this matches your backend port
 });
 
 export default instance;
